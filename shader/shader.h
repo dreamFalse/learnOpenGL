0b0641a857cb1f15848e202cc70dd547810b8c41
fenghh <fenghuihang@foxmail.com>
2026-01-19 23:00:27 +0800

    add shader.h

--- shader/shader.h
+++ shader/shader.h
@@ -62,12 +62,52 @@ Shader::Shader(const char* vertexPath, const char* fragmentPath)
     char infoLog[512]; 
 
     // 顶点着色器
-    
+    vertex = glCreateShader(GL_VERTEX_SHADER); 
+    glShaderSource(vertex, 1, &vShaderCode, NULL); 
+    glCompileShader(vertex); 
+    // 打印错误（如果有的话）
+    glGetShaderiv(vertex, GL_COMPILE_STATUS, &success); 
+    if (!success) {
+        glGetShaderInfoLog(vertex, 512, NULL, infoLog); 
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED" << infoLog << std::endl; 
+    }
+
+    // 片段着色器
+    fragment = glCreateShader(GL_FRAGMENT_SHADER); 
+    glShaderSource(fragment, 1, &fShaderCode, NULL); 
+    glCompileShader(fragment); 
+    glGetShaderiv(fragment, GL_COMPILE_STATUS, &success); 
+    if (!success) {
+        glGetShaderInfoLog(fragment, 512, NULL, infoLog); 
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED" << infoLog << std::endl; 
+    }
+
+    // 着色器程序
+    ID = glCreateProgram(); 
+    glAttachShader(ID, vertex); 
+    glAttachShader(ID, fragment); 
+    glLinkProgram(ID); 
+
+    // 删除着色器，它们已经链接到我们的程序中了，已经不再需要
+    glDeleteShader(vertex); 
+    glDeleteShader(fragment); 
 }
 
 void Shader::use()
 {
     glUseProgram(ID); 
 }
+
+void Shader::setBool(const std::string &name, bool value) const {
+    glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value); 
+}
+void setInt(const std::string &name, int value) const
+{ 
+    glUniform1i(glGetUniformLocation(ID, name.c_str()), value); 
+}
+void setFloat(const std::string &name, float value) const
+{ 
+    glUniform1f(glGetUniformLocation(ID, name.c_str()), value); 
+} 
 
 #endif 
