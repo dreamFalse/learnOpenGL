99da28b715084636977b45f4b1049e145e46b264
fenghh <fenghuihang@foxmail.com>
2026-02-08 23:16:22 +0800

    finish load model

--- demo_lighting/main.cpp
+++ demo_lighting/main.cpp
@@ -109,6 +109,19 @@ int main()
         -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f
     };
 
+    glm::vec3 cubePositions[] = {
+        glm::vec3( 0.0f,  0.0f,  0.0f),
+        glm::vec3( 2.0f,  5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3( 2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f,  3.0f, -7.5f),
+        glm::vec3( 1.3f, -2.0f, -2.5f),
+        glm::vec3( 1.5f,  2.0f, -2.5f),
+        glm::vec3( 1.5f,  0.2f, -1.5f),
+        glm::vec3(-1.3f,  1.0f, -1.5f)
+    };
+
     unsigned int VBO, cubeVAO; 
     glGenVertexArrays(1, &cubeVAO); 
     glGenBuffers(1, &VBO); 
@@ -191,7 +204,16 @@ int main()
 
         // render the cube 
         glBindVertexArray(cubeVAO); 
-        glDrawArrays(GL_TRIANGLES, 0, 36); 
+        for(unsigned int i = 0; i < 10; i++) {
+            glm::mat4 model; 
+            model = glm::translate(model, cubePositions[i]); 
+            float angle = 20.0f * i; 
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f)); 
+            lightingShader.setMat4("model", model); 
+            
+            glDrawArrays(GL_TRIANGLES, 0, 36); 
+            
+        }
 
         // also draw the lamp object
         lightCubeShader.use(); 
